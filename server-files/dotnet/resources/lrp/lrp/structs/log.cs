c60697f6e14ff694413c2ab0b1dafe77b82fcdbb
nitro <casuallyimploding@gmail.com>
2020-10-22 11:46:20 +0300

    initial commit.

---
+++ server-files/dotnet/resources/lrp/lrp/structs/log.cs
@@ -0,0 +1,21 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lrp.structs
+{
+    public class log
+    {
+        public static void Log(string text, ConsoleColor cc = ConsoleColor.Red) {
+            string format = $"[LOG] {DateTime.Now} : {text}";
+
+            Console.BackgroundColor = cc;
+
+            NAPI.Util.ConsoleOutput(format);
+
+            Console.ResetColor();
+
+        }
+    }
+}
